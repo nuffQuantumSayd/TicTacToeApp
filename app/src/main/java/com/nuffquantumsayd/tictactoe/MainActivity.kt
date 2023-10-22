64c8e2e7b201bbb3c4677604d133e9e2eee7e99a
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-10-22 07:21:04 -0700

    All buttons are identified and game board buttons are in an list

--- app/src/main/java/com/nuffquantumsayd/tictactoe/MainActivity.kt
+++ app/src/main/java/com/nuffquantumsayd/tictactoe/MainActivity.kt
@@ -11,6 +11,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //identify other buttons to set with
+        //top row
+        val topLeftButton = findViewById<Button>(R.id.top_left)
+        val topMiddleButton = findViewById<Button>(R.id.top_middle)
+        val topRightButton = findViewById<Button>(R.id.top_right)
+        //middle row
+        val middleLeftButton = findViewById<Button>(R.id.middle_left)
+        val middleMiddleButton = findViewById<Button>(R.id.the_middle)
+        val middleRightButton = findViewById<Button>(R.id.middle_right)
+        //bottom row
+        val bottomLeftButton = findViewById<Button>(R.id.bottom_left)
+        val bottomMiddleButton = findViewById<Button>(R.id.bottom_middle)
+        val bottomRightButton = findViewById<Button>(R.id.bottom_right)
+
+        //put buttons into arraylist of buttons
+        val gameBoardButtons = listOf<Button>(topLeftButton, topMiddleButton, topRightButton,
+                                                middleLeftButton, middleMiddleButton, middleRightButton,
+                                               bottomLeftButton, bottomMiddleButton, bottomRightButton)
+
 
         //Identify new game button and set a listener for it
         val newGameButton = findViewById<Button>(R.id.new_game)
@@ -20,7 +38,8 @@ class MainActivity : AppCompatActivity() {
 
     }
     private fun newGamePressed(){
-        Toast.makeText(this,"new game button was pressed!!", Toast.LENGTH_SHORT).show()
+        val toastText = "new game was pressed"
+        Toast.makeText(this,toastText, Toast.LENGTH_SHORT).show()
     }
 
 }
